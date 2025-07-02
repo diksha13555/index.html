30b0e18f16ca96d25135ab898c65b76eb41628f3
Diksha Mahajan <diksha13905@gmail.com>
2025-07-02 12:57:19 +0530

    First comit

---
+++ 1/sign-script.js
@@ -0,0 +1,14 @@
+function searchProducts() {
+  let input = document.getElementById('searchInput').value.toLowerCase();
+  let products = document.getElementsByClassName('product-card');
+
+  for (let i = 0; i < products.length; i++) {
+    let productName = products[i].getAttribute('data-name').toLowerCase();
+
+    if (productName.includes(input)) {
+      products[i].style.display = 'block';
+    } else {
+      products[i].style.display = 'none';
+    }
+  }
+}
